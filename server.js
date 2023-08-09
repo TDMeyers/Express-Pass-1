1e87a16d0541bcd245f55561bbf1ebfa71c5bc2a
TDMeyers <meyers.dylan@outlook.com>
2023-08-08 21:12:11 -0600

    finish routing requirements

--- server.js
+++ server.js
@@ -1,71 +1,31 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
-const count = 99;
+
+const PORT = 8080;
 
 app.get("/", (req, res) => {
-  res.send(`
-    <h1>${count} Bottles of beer on the wall</h1>
-    <a href="http://localhost:${port}/${
-    count - 1
-  }">take one down, pass it around</a>
-    <br>
-    <a href="http://localhost:${port}/bonus">Bonus</a>
-  `);
+  res.send(
+    `<h3>There's 99 Bottles of beer on the wall~</h3><a href="/98">Take one down, pass it around...</a>`
+  );
 });
 
-app.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (id === 0) {
-    res.send(`
-      <h1>No more bottles of beer on the wall</h1>
-      <a href="http://localhost:${port}">Start over</a>
-    `);
-  } else {
-    res.send(`
-      <h1>${id} Bottles of beer on the wall</h1>
-      <a href="http://localhost:${port}/${
-      id - 1
-    }">take one down, pass it around</a>
-      <br>
-      <a href="http://localhost:${port}/bonus">Bonus</a>
-    `);
-  }
+app.get("/:number_of_bottles", (req, res) => {
+    if (req.params.number_of_bottles <= 0 ) {
+      res.send(
+        `<h1>Please call or text the 24/7 Addiction Helpline at 331-227-5290.</h1><h3>There are no more bottles of beer on the wall.</h3><a href="/">Starting over?</a>`
+      );
+    }
+    else {
+      res.send(`<h3>There's ${
+        req.params.number_of_bottles
+      } Bottles of beer on the wall~</h3><a href='/${
+        req.params.number_of_bottles - 1
+      }'>Take one down, pass it around...</a>`
+    );
+    }
 });
 
-
-//===============================================================
-
-// app.get("/", (req, res) => {
-//   res.send(
-//     `<h1>${count} Bottles of beer on the wall</h1> <a href=http://localhost:${port}/${
-//       count - 1
-//     }>take one down, pass it around</a>`
-//   );
-// });
-
-// app.get("/:id", (req, res) => {
-//   if (!req.params.id === 0) {
-//     res.send(
-//       `<h1>${
-//         req.params.id
-//       } Bottles of beer on the wall</h1><a href=http://localhost:${port}/${
-//         req.params.id - 1
-//       }>take one down, pass it around</a>`
-//     );
-//   } else {
-//     res.send(
-//       `<h1>${
-//         req.params.id
-//       } Bottles of beer on the wall</h1><a href=http://localhost:${port}/${
-//         req.params.id - 1
-//       }>take one down, pass it around</a>`
-//     );
-//   }
-// });
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+app.listen(PORT, ()=>{
+  console.log('starting server at port: ' + PORT)
+})
